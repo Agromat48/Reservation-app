177703ee120a2981c70d5096f1b100ab9216ab0a
Matvey <matvey.shatalin.07@gmail.com>
2026-09-24 17:10:18 +0300

    override equals and hashCode in ReservationEntity

--- src/main/java/com/example/reservation/reservations/ReservationEntity.java
+++ src/main/java/com/example/reservation/reservations/ReservationEntity.java
@@ -94,4 +94,16 @@ public class ReservationEntity {
     public Long getId() {
         return id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ReservationEntity that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
